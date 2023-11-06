f0f5e3997667f6d8e56f93621a337f28b69ab0e5
Ostap Chichiginarov <ostap00034@gmail.com>
2023-11-06 16:56:52 +0900

    fix

--- src/app/api/page.tsx
+++ src/app/api/page.tsx
@@ -1,14 +1,16 @@
-import { createNextApiHandler } from '@next/api'
+// файл: pages/api/switch.ts
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default createNextApiHandler({
-	async GET(req, res) {
-		const { state } = req.query
-		// Здесь ваша логика для чтения состояния светодиода
-		res.json({ state: state || 'off' })
-	},
-	async POST(req, res) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method === 'POST') {
+		// Обработка POST запроса
 		const { state } = req.body
-		// Здесь ваша логика для установки состояния светодиода
-		res.json({ status: 'success', state })
-	},
-})
+		// Тут логика для изменения состояния светодиода
+		console.log(`Switch state is set to: ${state}`)
+		res.status(200).json({ status: 'success', state })
+	} else {
+		// Обработка для других методов, например, GET
+		res.setHeader('Allow', ['POST'])
+		res.status(405).end(`Method ${req.method} Not Allowed`)
+	}
+}
